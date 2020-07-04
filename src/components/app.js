64350ded6cbdccaac263064233594d50986f22c4
LivingMartialWay <LivingMartialWay@gmail.com>
2020-07-04 20:53:51 +0300

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import unsplash from './api/unsplash';
+import SearchBar from '@bit/livingmartialway.bitjrdevapp.search-bar';
+import Logo from '@bit/livingmartialway.bitjrdevapp.logo';
+import ImageList from '@bit/livingmartialway.bitjrdevapp.image-list';
+import './app.css'
+
+class App extends React.Component {
+  state = { images: [] };
+
+   onTermSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
+
+    });
+
+    this.setState({ images: response.data.results });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <legend className="sblegend">bit.dev/livingmartialway/bitjrdevapp/search-bar
+          <SearchBar onFormSubmit={this.onTermSubmit}/>
+            <legend className="logolegend">bitjrdevapp/logo</legend>
+        </legend>
+
+        <legend className="imagelistlegend">bit.dev/livingmartialway/bitjrdevapp/image-list  & image-card
+          <ImageList images={this.state.images} />
+        </legend>
+      </div>
+    );
+  }
+}
+export default App;
